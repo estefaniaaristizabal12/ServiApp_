36b82b268b7f5e8c08dacbc79249a15fa3d4946d
estefaniaaristizabal12 <estefaniaaristizabal12@hotmail.com>
2022-08-28 23:32:36 -0500

    restaurante feo

--- src/screens/restScreens/Restaurant.tsx
+++ src/screens/restScreens/Restaurant.tsx
@@ -1,16 +1,79 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { View } from 'react-native-animatable'
+import { Dimensions, SectionList, StyleSheet, Text } from 'react-native'
+import { Image, View } from 'react-native-animatable'
 import Button from '../../components/Button'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+
+
+const DATA = [
+    {
+        title: "Main dishes",
+        data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+        title: "Sides",
+        data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+        title: "Drinks",
+        data: ["Water", "Coke", "Beer"]
+    },
+    {
+        title: "Desserts",
+        data: ["Cheese Cake", "Ice Cream"]
+    }
+];
+
+
+const Item = ({ title }) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
 
 export const Restaurant = ({ navigation }) => {
     const { top: paddingTop } = useSafeAreaInsets();
     return (
-        <View style={{paddingTop}}>
+        <View style={{ paddingTop }}>
+
+            <Image
+                style={styles.logo}
+                source={require('../../../assets/pizza.jpg')}
+            />
+
             <Text> RESTAURANTE </Text>
             <Button title="Product" onPress={() => navigation.navigate('Product')} />
+
+            <SectionList
+                sections={DATA}
+                keyExtractor={(item, index) => item + index}
+                renderItem={({ item }) => <Item title={item} />}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text style={styles.header}>{title}</Text>
+                )}
+            />
 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    logo: {
+        width: Dimensions.get('window').width,
+        height: 200
+    },
+    item: {
+      backgroundColor: "#f9c2ff",
+      padding: 20,
+      marginVertical: 8
+    },
+    header: {
+      fontSize: 32,
+      backgroundColor: "#fff"
+    },
+    title: {
+      fontSize: 24
+    }
+  });
+  
