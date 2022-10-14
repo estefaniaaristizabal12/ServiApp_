aba508045633da0e4055d9dedf414eb340a3e03e
estefaniaaristizabal12 <estefaniaaristizabal12@hotmail.com>
2022-10-14 11:59:07 -0500

    cambios de carro de compras

--- src/components/CardCart.tsx
+++ src/components/CardCart.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Text, Image, View, StyleSheet, Dimensions } from "react-native";
 import { normalize } from '../../FontNormalize';
+import { FontAwesome } from '@expo/vector-icons';
+import { Colors } from '../constants/colors';
 
 const { width, height } = Dimensions.get("screen");
 
@@ -12,7 +14,7 @@ export const CardCart = ({ title, precio, cantidad, image }) => {
                 <View style={{ flex: 0.3 }}>
                     <Image
                         style={{ width: "100%", height: "100%", borderRadius: 20, marginLeft: 5 }}
-                        source={{uri: image}}
+                        source={{ uri: image }}
                     />
                 </View>
 
@@ -20,20 +22,18 @@ export const CardCart = ({ title, precio, cantidad, image }) => {
                     flex: 0.65
                     , marginHorizontal: 12, overflow: "hidden"
                 }}>
-                    <Text style={style.titulo}>{title}</Text>
-                    <Text style={style.precio}>${precio}</Text>
 
-                    <View style={{ alignItems: "flex-end", marginTop: 15, padding: 5 }}>
-                        <View style={style.btnModificar}>
-                            <TouchableOpacity >
-                                <Text style={style.textBtn}>-</Text>
-                            </TouchableOpacity>
-                            <Text style={style.textBtn}>{cantidad}</Text>
-                            <TouchableOpacity >
-                                <Text style={style.textBtn}>+</Text>
-                            </TouchableOpacity>
+                    <View style={{ flexDirection: "row", alignItems: "center", flex: 1 }}>
+                        <View style={{ flexDirection: "column", flex: 0.8 }}>
+                            <Text style={style.titulo}>{title}</Text>
+                            <Text style={style.precio}>${precio}</Text>
                         </View>
+                        <View style={{ marginVertical: 5, alignItems: "center", justifyContent:"center", flex: 0.2, backgroundColor:Colors.secondary1, borderRadius:50 }}>
+                            <FontAwesome style={{marginVertical:8}} name="trash" size={25} color={Colors.primary1} />
+                        </View>
+
                     </View>
+
                 </View>
             </View>
         </View>
@@ -67,7 +67,7 @@ const style = StyleSheet.create({
         flex: 0.3,
     },
     titulo: {
-        fontSize: normalize(18),
+        fontSize: normalize(20),
         color: '#000000',
         marginLeft: 10,
     },
@@ -79,7 +79,7 @@ const style = StyleSheet.create({
         marginLeft: 10,
     },
     precio: {
-        fontSize: normalize(15),
+        fontSize: normalize(18),
         fontWeight: 'bold',
         marginLeft: 10,
         marginTop: 5
